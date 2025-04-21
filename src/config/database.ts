60cc6bbd6ac6a52a3aaff9370c7b08cdf3639085
Marcelo Plínio <mplmdswork@gmail.com>
2025-04-21 16:08:12 -0300

    fix: Disable prisma executeRaw

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
-prisma.$executeRaw`PRAGMA foreign_keys = ON;`;
+// prisma.$executeRaw`PRAGMA foreign_keys = ON;`;
 
