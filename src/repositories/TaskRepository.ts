e721cd7a26b4728ab72e32f4dc3948e2f977dc73
Marcelo Plínio <mplmdswork@gmail.com>
2025-04-21 15:24:24 -0300

    Fix: Correcting status error when ID does not exist in PUT

--- src/repositories/TaskRepository.ts
+++ src/repositories/TaskRepository.ts
@@ -26,11 +26,19 @@ class TaskRepository {
   }
 
   async update(id: number, data: any) {
-    return await prisma.task.update({
-      where: { id },
-      data,
-    });
+    try {
+      return await prisma.task.update({
+        where: { id },
+        data,
+      });
+    } catch (error: any) {
+      if (error.code === 'P2025') {
+        return null; 
+      }
+      throw error; 
+    }
   }
+  
 
   async delete(id: number) {
     return await prisma.task.delete({
